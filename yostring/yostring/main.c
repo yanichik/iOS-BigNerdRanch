38b26b2730c9346d6303249a24d6812918af495a
Yan <brunshte@gmail.com>
2023-12-02 01:13:14 -0800

    yostring init commit

---
+++ yostring/yostring/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  yostring
+//
+//  Created by YB on 12/2/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
