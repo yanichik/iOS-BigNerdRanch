ad2a7e06f9be4a0afa0eb4bb6ba5b8be78c3474a
Yan <brunshte@gmail.com>
2024-01-10 23:46:01 -0800

    add Quiz app

---
+++ Quiz/Quiz/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by YB on 12/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = [
+        "What is 7+7?",
+        "What is the capital of Vermont?",
+        "What is cognac made from?"
+    ]
+    let answers: [String] = [
+        "14",
+        "Montpelier",
+        "Grapes"
+    ]
+    var currentQuestionIndex: Int = 0
+    @IBAction func showNextQuestion(_ sender: UIButton){
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
+    @IBAction func showAnswer(_ sender: UIButton){
+        let answer = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questions[currentQuestionIndex]
+    }
+}
+
