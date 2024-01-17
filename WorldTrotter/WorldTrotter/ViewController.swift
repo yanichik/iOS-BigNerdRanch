7a5cee39ba2c6918debaf77eb0e4ca528dc0f208
Yan <brunshte@gmail.com>
2024-01-17 07:46:08 -0800

    move gradient code to viewDidLoad and gradientLayer as property of class

--- WorldTrotter/WorldTrotter/ViewController.swift
+++ WorldTrotter/WorldTrotter/ViewController.swift
@@ -8,24 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var gradientLayer: CAGradientLayer = CAGradientLayer()
     override func viewDidLoad() {
-//        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
-//        let firstView = UIView(frame: firstFrame)
-//        firstView.backgroundColor = UIColor.blue
-//        view.addSubview(firstView)
-//
-//        let secondFrame = CGRect(x: 20, y: 30, width: 150, height: 150)
-//        let secondView = UIView(frame: secondFrame)
-//        secondView.backgroundColor = UIColor.green
-//        firstView.addSubview(secondView)
-//        view.addSubview(secondView)
-    }
-    
-    override func viewWillLayoutSubviews() {
-        let gradientLayer = CAGradientLayer()
         gradientLayer.colors = [UIColor.red.cgColor, UIColor.yellow.cgColor, UIColor.green.cgColor, UIColor.blue.cgColor]
         gradientLayer.transform = CATransform3DMakeRotation(CGFloat.pi, 0, 0, 1)
         gradientLayer.frame = view.bounds
         view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
     }
 }
