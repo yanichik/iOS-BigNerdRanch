2aedf972a59e6a864292bc2118aa8d0feeca82a5
Yan <brunshte@gmail.com>
2024-01-16 18:25:54 -0800

    init World Trotter

---
+++ WorldTrotter/WorldTrotter/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by YB on 1/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+//        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
+//        let firstView = UIView(frame: firstFrame)
+//        firstView.backgroundColor = UIColor.blue
+//        view.addSubview(firstView)
+//
+//        let secondFrame = CGRect(x: 20, y: 30, width: 150, height: 150)
+//        let secondView = UIView(frame: secondFrame)
+//        secondView.backgroundColor = UIColor.green
+//        firstView.addSubview(secondView)
+//        view.addSubview(secondView)
+    }
+}
