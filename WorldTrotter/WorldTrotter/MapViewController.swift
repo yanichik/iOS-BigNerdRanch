34db4e31c2cad372af604c1db238cb4a078f9f53
Yan <brunshte@gmail.com>
2024-01-24 16:39:28 -0800

    remove map view from storyboard, add back in programmatically, setup segmented control with constraints

--- WorldTrotter/WorldTrotter/MapViewController.swift
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -6,8 +6,40 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+    
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        super.loadView()
+        mapView = MKMapView()
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor = UIColor.systemBackground
+        segmentedControl.selectedSegmentIndex = 2
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view = mapView
+        view.addSubview(segmentedControl)
+        
+        // Instead of setting top anchor constraint to the view's topAnchor, setting to offset of view's topAnchor based off of safeAreaLayoutGuide
+        // let topConstraint = segmentedControl.topAnchor.constraint(equalTo: view.topAnchor)
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8)
+        
+        // Instance of view's margins guide
+        let margins = view.layoutMarginsGuide
+        
+        // Instead of using view's leading and trailing anchor, setup constraints based on margins guide
+        // let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+        // let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
