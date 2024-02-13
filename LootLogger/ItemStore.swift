dead04592a5e135109251bdc0b3d1343adbecc8c
Yan <brunshte@gmail.com>
2024-02-13 08:18:32 -0800

    init LootLogger app, add editing mode toggle button

---
+++ LootLogger/ItemStore.swift
@@ -0,0 +1,27 @@
+//
+//  ItemStore.swift
+//  LootLogger
+//
+//  Created by YB on 2/12/24.
+//
+
+import UIKit
+
+class ItemStore {
+    var allItems = [Item]()
+    
+    @discardableResult func createItem() -> Item{
+        var randomItem = Item(random: true)
+        allItems.append(randomItem)
+        return randomItem
+    }
+    
+    init() {
+        // This is why you annotated createItem() with @discardableResult. If you had not, then the call to
+        // that function would have needed to look like where you call the function, but ignore the result:
+            // let _ = createItem()
+        for _ in 0..<25{
+            createItem()
+        }
+    }
+}
