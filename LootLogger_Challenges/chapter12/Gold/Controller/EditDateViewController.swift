83351476f8758338a63e3c51d7a21840a77b4bf3
Yan <brunshte@gmail.com>
2024-02-28 10:01:57 -0800

    create EditDateVC

--- LootLogger_Challenges/chapter12/Gold/Controller/EditDateViewController.swift
+++ LootLogger_Challenges/chapter12/Gold/Controller/EditDateViewController.swift
@@ -8,11 +8,33 @@
 import UIKit
 
 class EditDateViewController: UIViewController {
-
+    
+    var datePicker: UIDatePicker!
+    var item: Item!
+    
+    init(datePicker: UIDatePicker!, item: Item) {
+        super.init(nibName: nil, bundle: nil)
+        self.datePicker = datePicker
+        self.item = item
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        datePicker.date = item.dateCreated
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("entered change date VC")
+        print(datePicker.date)
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        item.dateCreated = datePicker.date
     }
     
 
